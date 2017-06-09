dae37c798044d2fe941c49651bb5b394aa1d4442
Viesturs Teivāns <vteivans@gmail.com>
2017-06-10 01:21:04 +0300

    Code adjustment

--- Cloud File direct URL/popup.js
+++ Cloud File direct URL/popup.js
@@ -1,111 +1,75 @@
-
-
 window.onload = function () {
-    var urlContainer = document.getElementById("url-container");
-    var errorView = document.getElementById('error'),
-        resultView = document.getElementById('result'),
-        introView = document.getElementById('intro');
+  var urlContainer = document.getElementById("url-container");
+  var errorView = document.getElementById('error'),
+      resultView = document.getElementById('result'),
+      introView = document.getElementById('intro');
+  var resultOutput = resultView.querySelector('input'),
+      copyButton = resultView.querySelector('button');
 
-    // First we get the active tab - the one user is looking at right now:
-    chrome.tabs.query({
-        active: true,               // Select active tabs
-        lastFocusedWindow: true     // In the current window
-    },
-    function (tabs) {
-        // Since there can only be one active tab in one active window,
-        //  the array has only one element
-        var tab = tabs[0];
+  // First we get the active tab - the one user is looking at right now:
+  chrome.tabs.query({
+    active: true,               // Select active tabs
+    lastFocusedWindow: true     // In the current window
+  },
+  function (tabs) {
+    // Get the current tab:
+    var tab = tabs[0];
 
-        if (!tab) {
-           showError('No active tab');
-            return;
-        }
-        var embedURL = createGoogleDriveURL(tab.url);
+    // Generate URL for Google Drive
+    var embedURL = createGoogleDriveURL(tab.url);
 
-        if (!embedURL) {
-            showError("URL of current tab isn't Google drive file URL!");
-            return;
-        }
+    if (!embedURL) {
+      showError("URL of current tab isn't Google drive file URL!");
+      return;
+    }
 
-        // Display URL
-        showResult(embedURL);
+    // Hide error and intro information:
+    errorView.classList.remove('error');
+    introView.classList.remove('intro');
 
-        /*
-        input.focus();
+    // When input gets clicked, select the URL
+    resultOutput.addEventListener('click',
+      function clickURLInputOutput (event) {
+        resultOutput.focus();
         setTimeout(() => {
-            input.setSelectionRange(0, input.value.length);
+          resultOutput.setSelectionRange(0, resultOutput.value.length);
         }, 0);
-        */
+      }, null);
+
+    // When copy button gets clicked, copy the URL and close the popup
+    copyButton.addEventListener('click',
+      function clickCopyURLButton (event) {
+        resultOutput.focus();
+        setTimeout(() => {
+          resultOutput.setSelectionRange(0, resultOutput.value.length);
+          document.execCommand('copy');
+          window.close();
+        }, 0);
+      }, null);
+
+    // Show the result
+    resultOutput.value = embedURL;
+    resultView.classList.add('result');
+  });
 
 
-    });
+  function showError(message) {
+    resultView.classList.remove('result');
+    introView.classList.remove('intro');
 
-    function showError(message) {
-        if (resultView) {
-            resultView.classList.remove('result');
-        }
-        if (introView) {
-            introView.classList.remove('intro');
-        }
-
-        if (!errorView) {
-            console.error("No error view! Can't show error: " + message);
-            return;
-        }
-        errorView.querySelector('p').innerHTML = message;
-        errorView.classList.add('error');
-    }
-
-    function showResult(result) {
-        if (errorView) {
-            errorView.classList.remove('error');
-        }
-        if (introView) {
-            introView.classList.remove('intro');
-        }
-
-        if (!resultView) {
-            showError('No result view!');
-            return;
-        }
-
-        var resultOutput = resultView.querySelector('input');
-        var copyButton = resultView.querySelector('button');
-        if (!resultOutput) {
-            showError('No result output!');
-            return;
-        }
-
-        resultOutput.value = result;
-
-        resultOutput.addEventListener('click', function (event) {
-            resultOutput.focus();
-            setTimeout(() => {
-                resultOutput.setSelectionRange(0, resultOutput.value.length);
-            }, 0);
-        }, null);
-
-        if (copyButton) {
-            copyButton.addEventListener('click', function (event) {
-                resultOutput.focus();
-                setTimeout(() => {
-                    resultOutput.setSelectionRange(0, resultOutput.value.length);
-                    document.execCommand('copy');
-                }, 0);
-            }, null);
-        }
-
-        resultView.classList.add('result');
-    }
+    errorView.querySelector('p').innerHTML = message;
+    errorView.classList.add('error');
+  }
 };
 
+
 function createGoogleDriveURL(url) {
-    var fileIDRegEx = /drive\.google\.com\/file\/d\/([^\/]+)/;
-    var urlFragments = String(url).match(fileIDRegEx);
+  var fileIDRegEx = /drive\.google\.com\/file\/d\/([^\/]+)/;
+  var urlFragments = String(url).match(fileIDRegEx);
 
-    if (urlFragments.length < 2) {
-        return;
-    }
+  if (urlFragments.length < 2) {
+    return;
+  }
 
-    return 'https://drive.google.com/uc?export=view&id=' + urlFragments[1];
+  return 'https://drive.google.com/uc?export=view&id=' + urlFragments[1];
 }
